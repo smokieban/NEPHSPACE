5d434d3c27217d60cd05dd95be6b5b4a75239091
smokieban <127643069+smokieban@users.noreply.github.com>
2026-04-17 12:21:45 +0300

    neph15

--- testimonials-admin.php
+++ testimonials-admin.php
@@ -68,7 +68,7 @@ function testimonialsAdminDate($value) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="icon" type="image/svg+xml" href="assets/logo/nephspace-favicon.svg">
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" sizes="any">
 </head>
 <body class="bg-light">
     <div class="container py-5">
